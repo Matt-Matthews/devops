8f527a0490a0011284c3ba90b25dbcb0d387535a
Fani2000 <keorapetsefani2000@gmail.com>
2023-05-24 13:01:42 +0200

    Added the populate function to ensure that the seed data is populate into your local environment before using

--- server/index.js
+++ server/index.js
@@ -1,26 +1,20 @@
 const http = require("http");
 const express = require("express");
 const dotenv = require("dotenv").config();
-const {db} = require('../arango');
-const mod_AOI_collection = require('../arango/collections/db_Aoi');
-const {aoiCollections} = require('../arango/collections/')
-const {readMail} = require('./readMail');
+const { db } = require("../arango");
+const DbAoiCollection = require("../arango/collections/db_Aoi");
+const { aoiCollections } = require("../arango/collections/");
+const { readMail } = require("./readMail");
 // main()
 const app = express();
 
-app.get('/', async (req, res) => {
-    res.send('Welcome to the main backend!!!!')
-    // mod_aoi.createAOICollection();
-    // res.json(emailList)
-    });
-    
-    
-//     async function main () {
-        
-//         const emailList = await readMail();
+app.get("/", async (req, res) => {
 
-//         console.table(emailList);
-// }
+  const emailList = await readMail();
+
+  console.table(emailList);
+  res.json(emailList);
+});
 
 const server = http.createServer(app);
 
--- server/readMail.js
+++ server/readMail.js
@@ -1,47 +1,54 @@
-const imaps = require('imap-simple');
-require('dotenv').config()
+const imaps = require("imap-simple");
+require("dotenv").config();
 
-const {EMAIL_SERVER, OUTGOING_EMAIL_PORT,CAM_ALERT_EMAIL_USER,CAM_ALERT_EMAIL_PASSWORD } = process.env;
+const {
+  EMAIL_SERVER,
+  OUTGOING_EMAIL_PORT,
+  CAM_ALERT_EMAIL_USER,
+  CAM_ALERT_EMAIL_PASSWORD,
+} = process.env;
 
 const readMailConfig = {
-    imap: {
-        user: CAM_ALERT_EMAIL_USER,
-        password: CAM_ALERT_EMAIL_PASSWORD,
-        host: EMAIL_SERVER,
-        port: Number(OUTGOING_EMAIL_PORT),
-        authTimeout: 10000,
-        tls: true,
-        tlsOptions:{
-            rejectUnauthorized: false,
-        }
-    }
+  imap: {
+    user: CAM_ALERT_EMAIL_USER,
+    password: CAM_ALERT_EMAIL_PASSWORD,
+    host: EMAIL_SERVER,
+    port: Number(OUTGOING_EMAIL_PORT),
+    authTimeout: 10000,
+    tls: true,
+    tlsOptions: {
+      rejectUnauthorized: false,
+    },
+  },
 };
 
-console.log(readMailConfig)
-
 async function readMail() {
+  let emailList = [];
 
-    let emailList = [];
+  try {
+    const connection = await imaps.connect(readMailConfig);
 
-    try{
-        const connection = await imaps.connect(readMailConfig);
-        await connection.openBox('INBOX');
-        const fetchOptions = {
-            bodies: ['HEADER', 'TEXT'],
-            markSeen: false
-        };
-        const results = await connection.search(['ALL'], fetchOptions);
-        results.forEach(res => {
-            const body = res.parts.filter(part=>part.which === 'TEXT')[0].body;
-            if(!body.includes('Event type - SMS')) emailList.push(JSON.parse(body));
-        });
+    await connection.openBox("INBOX");
 
-        return emailList;
-    }catch(err){
-        console.log(err);
-    } 
+    const fetchOptions = {
+      bodies: ["HEADER", "TEXT"],
+      markSeen: false,
+    };
+
+    const results = await connection.search(["ALL"], fetchOptions);
+
+    results.forEach((res) => {
+      const body = res.parts.filter((part) => part.which === "TEXT")[0].body;
+
+      if (!body.includes("Event type - SMS")) emailList.push(JSON.parse(body));
+    });
+
+    return emailList;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports = {
-    readMail,
-}
+  readMail,
+};
