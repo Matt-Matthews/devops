1a16c8212520fd9a9b3ec9aee614ad66954e8e23
Mathews Paku <mathewspaku@gmail.com>
2023-05-25 16:59:57 +0200

    cleaned the files

--- arango/index.js
+++ arango/index.js
@@ -5,6 +5,14 @@ require("dotenv").config();
 
 const db = new arango.Database(process.env.DB_HOSTNAME);
 
+db.login(
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD
+).then((doc) => {
+  console.log('User Logged in..')
+}).catch(err => console.log(err))
+// const dbInstance = db.database("myDB");
+
 module.exports = {
   db,
 };
--- server/index.js
+++ server/index.js
@@ -4,16 +4,13 @@ const dotenv = require("dotenv").config();
 const { db } = require("../arango");
 const DbAoiCollection = require("../arango/collections/db_Aoi");
 const { aoiCollections } = require("../arango/collections/");
-const { readMail } = require("./readMail");
-// main()
+
 const app = express();
+
 
 app.get("/", async (req, res) => {
 
-  const emailList = await readMail();
-
-  console.table(emailList);
-  res.json(emailList);
+  res.json({ message: 'Welcome' });
 });
 
 const server = http.createServer(app);
